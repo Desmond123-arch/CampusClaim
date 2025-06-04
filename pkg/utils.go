373e0b52a4939898021fc6ed8d45406a6fe71ee5
Desmond123-arch <desmondopokumends2004@gmail.com>
2025-06-04 02:18:58 +0000

    verification endpoint setup

--- pkg/utils.go
+++ pkg/utils.go
@@ -1,6 +1,10 @@
 package pkg
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rand"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 
 func HashPassword(password string) (string, error) {
@@ -14,4 +18,22 @@ func HashPassword(password string) (string, error) {
 func VerifyHash(password string, hashstring string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashstring), []byte(password))
 	return err == nil
+}
+
+const otpChars = "1234567890"
+
+func GenerateOTP() (string, error) {
+	length := 6
+    buffer := make([]byte, length)
+    _, err := rand.Read(buffer)
+    if err != nil {
+        return "", err
+    }
+
+    otpCharsLength := len(otpChars)
+    for i := range length {
+        buffer[i] = otpChars[int(buffer[i])%otpCharsLength]
+    }
+
+    return string(buffer), nil
 }
