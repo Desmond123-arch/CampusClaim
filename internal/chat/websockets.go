8e59e016cf5ee7e7d88b74f2c986ac3d41520553
Desmond123-arch <desmondopokumends2004@gmail.com>
2025-06-20 03:15:31 +0000

    feat:chat base setup

---
+++ internal/chat/websockets.go
@@ -0,0 +1,93 @@
+package chat
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Desmond123-arch/CampusClaim/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ChatChannel struct {
+	ID           string   `bson:"_id,omitempty"`
+	Participants []string `bson:"participants"`
+	CreatedAt    int64    `bson:"created_at"`
+}
+
+var Clients = map[string]*websocket.Conn{}
+
+func WebSocketUpgradeMiddleware() fiber.Handler {
+	return func(c * fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return  c.Next()
+		}
+		return c.SendStatus(fiber.StatusUpgradeRequired)
+	}
+}
+
+
+type IncomingMessage struct {
+	ReceiverID string `json:"receiver_id"`
+	Message string `json:"message"`
+}
+
+func HandleWebSocket(c *websocket.Conn) {
+	userID := "abed67f3-7c37-4a2b-ad83-6f7dc4176249"
+	fmt.Println(userID)
+	Clients[userID] = c
+	defer delete(Clients, userID)
+
+	
+
+	for {
+		var msg IncomingMessage
+		if err := c.ReadJSON(&msg); err != nil {
+			log.Println("Websocket read error", err)
+			break
+		}
+		ctx := context.Background()
+		channelCol := models.GetCollection("channels")
+		filter := bson.M{"participants": bson.M{"$all": []string{userID, msg.ReceiverID}}}
+		
+		var channel models.ChatChannel
+		err := channelCol.FindOne(ctx, filter).Decode(&channel)
+		if err != nil {
+			channel = models.ChatChannel{
+				ID: primitive.NewObjectID().Hex(),
+				Participants: []string{userID, msg.ReceiverID},
+				CreatedAt: time.Now().Unix(),
+			}
+			_, err = channelCol.InsertOne(ctx, channel)
+			if err != nil {
+				log.Println("error creating channel:", err)
+				continue
+			}
+		}
+
+		message := models.Messages{
+			ID: primitive.NewObjectID().Hex(),
+			ChannelID: channel.ID,
+			Sender: userID,
+			Receiver: msg.ReceiverID,
+			Content: msg.Message,
+			TimeStamp: time.Now().Unix(),
+		}
+		msgCol := models.GetCollection("messages")
+		_, err = msgCol.InsertOne(ctx, message)
+
+		if err != nil {
+			log.Println("Error saving message", err)
+			continue
+		}
+		if conn, ok := Clients[msg.ReceiverID]; ok {
+			conn.WriteJSON(message)
+		}
+	}
+
+}
--- models/database.go
+++ models/database.go
@@ -4,14 +4,18 @@ import (
 	"os"
 
 	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/mongo"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 var RedisClient *redis.Client
+var MDB *mongo.Client
 func Init() {
 	postgres_url := os.Getenv("POSTGRES_URL")
+	mongodb_url := os.Getenv("MONGODB_URL")
+
 	var err error
 	DB, err = gorm.Open(postgres.New(postgres.Config{
 		DSN:postgres_url,
@@ -24,7 +28,10 @@ func Init() {
         panic("failed to connect database")
     }
 	Setup(DB)
-
+	MDB, err = MongoSetup(mongodb_url)
+	if err != nil {
+        panic("failed to connect database")
+    }
 	RedisClient = redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 		Password: "",
