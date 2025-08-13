a1d3514de6fe548b2ce32a0a9d6ad9765c6eeb7c
Desmond123-arch <desmondopokumends2004@gmail.com>
2025-08-13 15:15:35 +0000

    tsquery update

--- pkg/recommends.go
+++ pkg/recommends.go
@@ -1,21 +1,26 @@
 package pkg
 
 import (
+	"strings"
+
 	"github.com/Desmond123-arch/CampusClaim/models"
 )
 
 /*
 * get all 10 recent users that have searched this keyword
  */
-func GetRecentSearched(ItemTitle string) ([]string, error) {
-	var tokens []string
+ func GetRecentSearched(ItemTitle string) ([]string, error) {
+    var tokens []string
 
-	err := models.DB.
-		Table("recent_searches rs").
-		Select("DISTINCT ut.token").
-		Joins("JOIN user_tokens ut ON ut.user_id = rs.posted_by").
-		Where("rs.search_tsv @@ to_tsquery('english', ?) AND ut.is_subscribed = true", ItemTitle).
-		Pluck("ut.token", &tokens).Error
+    // Sanitize ItemTitle for to_tsquery
+    tsQuery := strings.ReplaceAll(ItemTitle, " ", " & ")
 
-	return tokens, err
+    err := models.DB.
+        Table("recent_searches rs").
+        Select("DISTINCT ut.token").
+        Joins("JOIN user_tokens ut ON ut.user_id = rs.posted_by").
+        Where("rs.search_tsv @@ to_tsquery('english', ?) AND ut.is_subscribed = true", tsQuery).
+        Pluck("ut.token", &tokens).Error
+
+    return tokens, err
 }
