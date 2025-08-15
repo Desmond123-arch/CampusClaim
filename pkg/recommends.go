b46cf8355c9ba3e71dbfc0fe4c1aeeb25f6b21ef
Desmond123-arch <desmondopokumends2004@gmail.com>
2025-08-15 05:19:11 +0000

    all words search

--- pkg/recommends.go
+++ pkg/recommends.go
@@ -10,14 +10,14 @@ import (
 func GetRecentSearched(ItemTitle string) ([]string, error) {
 	var tokens []string
 
-	searchQuery := strings.ReplaceAll(ItemTitle, " ", " & ")
+	searchQuery := strings.ReplaceAll(ItemTitle, " ", " | ")
 	fmt.Println("Search Query:", searchQuery)
 	
 	err := models.DB.Raw(`
 		SELECT DISTINCT ut.token
 		FROM recent_searches rs
 		JOIN user_tokens ut ON ut.user = rs.posted_by
-		WHERE rs.search_tsv @@ to_tsquery('english', ?) 
+		WHERE rs.search_tsv @@ to_tsquery('english', ?)  
 		AND ut.is_subscribed = true
 		LIMIT 10
 	`, searchQuery).Scan(&tokens).Error
